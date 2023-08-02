f42d90c0ff3c64b511a79093652e8c632fc81a1b
gyp03236 <gyp03236@naver.com>
2023-08-02 23:28:50 +0900

    feat: InputView 추가

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,24 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+	private static final Scanner scanner;
+	private static final String CAR_NAME_INPUT_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).";
+	private static final String TRIAL_NUMBER_INPUT_MESSAGE = "시도할 회수는 몇회인가요?";
+
+	static  {
+		scanner = new Scanner(System.in);
+	}
+
+	public static String getCarName() {
+		System.out.println(CAR_NAME_INPUT_MESSAGE);
+		return scanner.nextLine();
+	}
+
+	public static int getTrialNumber() {
+		System.out.println(TRIAL_NUMBER_INPUT_MESSAGE);
+		return scanner.nextInt();
+	}
+
+}
